6ea272c0b6d892d0dedc8e5a9d22edc828a96c3d
chetana47 <chetanareddy64@gmail.com>
2023-04-02 21:27:21 -0400

    change

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -12,14 +12,14 @@ export const deleteTuit = async (tid) => {
         .delete(`${TUITS_API}/${tid}`)
     return response.data
 }
+
+export const createTuit = async (tuit) => {
+    const response = await axios.post(TUITS_API, tuit)
+    return response.data;
+}
 export const updateTuit = async (tuit) => {
     const response = await axios
         .put(`${TUITS_API}/${tuit._id}`, tuit);
     console.log(response);
     return tuit;
-}
-
-export const createTuit = async (tuit) => {
-    const response = await axios.post(TUITS_API, tuit)
-    return response.data;
 }
